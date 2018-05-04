54318bd04e52a8a25a0590b7e6b5fdef945e1d10
Dima <leetsushi@gmail.com>
2018-05-04 16:34:13 +0300

    Ass 4 start

--- Assignment 4/src/Expression.java
+++ Assignment 4/src/Expression.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.Map;
+import java.util.List;
+/**
+ *
+ */
 public interface Expression {
+
+    double evaluate(Map<String, Double> assignment) throws Exception;
+
+    double evaluate() throws Exception;
+
+    List<String> getVariables();
+
+    String toString();
+
+    Expression assign(String var, Expression expression);
 }
--- Assignment 4/src/Num.java
+++ Assignment 4/src/Num.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Map;
 
-public class Num {
+/**
+ *
+ */
+public class Num implements Expression{
+
+    double number;
+
+    public Num (double number) {
+        this.number = number;
+    }
+
+    public double evaluate(Map<String, Double> assignment) throws Exception {
+        throw new Exception ("Num holds no variables");
+    }
+
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    public double evaluate() throws Exception{
+        return this.number;
+    }
+
+    /**
+     * @return Returns null since number isn't a variable. (?)
+     */
+    public List<String> getVariables() {
+        return null;
+    }
+
+    /**
+     * @return returns the number saved in the field as a string
+     */
+    public String toString() {
+        return Double.toString(number);
+    }
+
+
+    /**
+     *
+     * @param var
+     * @param expression
+     * @return
+     */
+    Expression assign(String var, Expression expression) {
+
+    }
 }
